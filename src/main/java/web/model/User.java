e75f1b2d81b59bd2bb94634f75a25b7d0557114d
Eduard Iakovlev <yakovlevadn@mail.ru>
2024-11-02 10:20:48 +0700

    доработать обработку лшибки введения одинаковых адресов

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,36 @@
+package web.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column//(name = "name")
+    private String name;
+
+    @Column//(name = "lastName")
+    private String lastName;
+
+    @Column(unique = true)//(name = "email", unique = true)
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String lastName, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
